aa55001d0f384fc3bf6757215fc5fb6632e04062
Czxy <60283985+Conzxy@users.noreply.github.com>
2021-06-28 19:31:27 +0800

    Delete test directory

--- test/test_type_traits.cc
+++
@@ -1,9 +0,0 @@
-#include "../include/type_traits.h"
-
-using namespace TinySTL;
-
-int main(){
-	static_assert(Is_same_v<Add_pointer_t<int()>, int(*)()>, "Add pointer failed\n");
-	static_assert(Is_same_v<Add_pointer_t<int() const volatile>, int() const volatile>,
-				  "Add_pointer have not effect in cv/ref qualified function type\n");
-}
