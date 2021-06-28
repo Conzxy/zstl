e585549044cc6422437f8b543d0fec84b2f2488b
Conzxy <1967933844@qq.com>
2021-06-28 20:16:51 +0800

    Revert "test"
    This reverts commit 77789a053bd0bb6c285f8f923b24ec33c1232e23.

---
+++ build/test/CMakeFiles/test_algo.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../../bin/test_algo"
+  "../../bin/test_algo.pdb"
+  "CMakeFiles/test_algo.dir/__/src/_alloc.cpp.o"
+  "CMakeFiles/test_algo.dir/test_algo.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_algo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
