203505799afccdf19bc25b0739fd7a67c7b2cec0
Conzxy <1967933844@qq.com>
2021-06-28 19:53:31 +0800

    Revert "Merge branch 'master' of https://github.com/Conzxy/TinySTL"
    This reverts commit 1273d08d529ef4b99c676f8a683c913efe00e44d, reversing
    changes made to e44255527061b1cd94aee033e70714ff970b6f37.

---
+++ build/test/CMakeFiles/test_other.dir/__/src/_alloc.cpp.obj.d
@@ -0,0 +1,27 @@
+test/CMakeFiles/test_other.dir/__/src/_alloc.cpp.obj: \
+ D:/Conzxy/TinySTL/src/_alloc.cpp d:\conzxy\tinystl\include\alloc.h \
+ c:\mingw\include\c++\9.2.0\cstddef \
+ c:\mingw\include\c++\9.2.0\x86_64-w64-mingw32\bits\c++config.h \
+ c:\mingw\include\c++\9.2.0\x86_64-w64-mingw32\bits\os_defines.h \
+ c:\mingw\include\c++\9.2.0\x86_64-w64-mingw32\bits\cpu_defines.h \
+ c:\mingw\include\c++\9.2.0\pstl\pstl_config.h \
+ c:\mingw\lib\gcc\x86_64-w64-mingw32\9.2.0\include\stddef.h \
+ c:\mingw\x86_64-w64-mingw32\include\stddef.h \
+ c:\mingw\x86_64-w64-mingw32\include\crtdefs.h \
+ c:\mingw\x86_64-w64-mingw32\include\corecrt.h \
+ c:\mingw\x86_64-w64-mingw32\include\_mingw.h \
+ c:\mingw\x86_64-w64-mingw32\include\_mingw_mac.h \
+ c:\mingw\x86_64-w64-mingw32\include\_mingw_secapi.h \
+ c:\mingw\x86_64-w64-mingw32\include\vadefs.h \
+ c:\mingw\x86_64-w64-mingw32\include\sdks/_mingw_directx.h \
+ c:\mingw\x86_64-w64-mingw32\include\sdks/_mingw_ddk.h \
+ c:\mingw\include\c++\9.2.0\cstdlib \
+ c:\mingw\x86_64-w64-mingw32\include\stdlib.h \
+ c:\mingw\x86_64-w64-mingw32\include\corecrt_wstdlib.h \
+ c:\mingw\lib\gcc\x86_64-w64-mingw32\9.2.0\include-fixed\limits.h \
+ c:\mingw\lib\gcc\x86_64-w64-mingw32\9.2.0\include-fixed\syslimits.h \
+ c:\mingw\x86_64-w64-mingw32\include\limits.h \
+ c:\mingw\x86_64-w64-mingw32\include\sec_api\stdlib_s.h \
+ c:\mingw\include\c++\9.2.0\stdlib.h \
+ c:\mingw\x86_64-w64-mingw32\include\malloc.h \
+ c:\mingw\include\c++\9.2.0\bits\std_abs.h
---
+++ test/tool.h
@@ -0,0 +1,27 @@
+//
+// Created by conzxy on 2021/3/20.
+//
+
+#ifndef TINYSTL__TOOL_H
+#define TINYSTL__TOOL_H
+
+#include <iostream>
+#include <random>
+
+template<typename Container>
+std::ostream& print_container(Container const& cont,std::string const& message="",
+                              std::ostream& os=std::cout){
+    os<<message;
+    for(auto& elem : cont)
+        os<<elem<<" ";
+    os<<std::endl;
+    return os;
+}
+
+auto getRandom(long long f,long long l){
+    static std::uniform_int_distribution<long long> u(f,l);
+    static std::default_random_engine e;
+    return u(e);
+}
+
+#endif //TINYSTL__TOOL_H
