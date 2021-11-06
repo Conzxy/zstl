d2a491a5df63840d69d2e6111b47836717871f33
Conzxy <1967933844@qq.com>
2021-11-06 16:04:53 +0800

    add tool for hashtable and rbtree

--- test/tool.h
+++ test/tool.h
@@ -1,13 +1,8 @@
-//
-// Created by conzxy on 2021/3/20.
-//
-
 #ifndef TINYSTL__TOOL_H
 #define TINYSTL__TOOL_H
 
 #include <iostream>
 #include <random>
-
 
 template<typename Container>
 std::ostream& PrintContainer(Container const& cont,std::string const& message="",
@@ -31,6 +26,31 @@ T GetRandom(T f,T l){
     static std::uniform_int_distribution<T> u(f,l);
     static std::default_random_engine e;
     return u(e);
+}
+
+std::string getRandomString(uint64_t len) {
+    static char const alphabetAndNumber[] = 
+        "abcdefghijklmnopqrstuvwxyz"
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        "0123456789";
+
+    static uint8_t index = sizeof alphabetAndNumber;
+
+    std::string str;
+    str.reserve(len);
+
+    while (len) {
+        str += alphabetAndNumber[GetRandom<int>(0, index)];
+        --len;
+    }
+
+    return str;
+}
+
+template<typename T, typename V>
+void emplace_backLoop(T& cont, unsigned n, V const& v) {
+	for (unsigned i = 0; i != n; ++i)
+		cont.emplace_back(v);
 }
 
 #endif //TINYSTL__TOOL_H
